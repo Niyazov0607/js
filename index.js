7bbcf1ea5b26de4690e85b271affbf86fe098b7e
Niyazov0607 <shohjahonniyazov89@gmail.com>
2024-12-04 19:22:48 +0500

    js

---
+++ index.js
@@ -0,0 +1,208 @@
+// Begin-1
+
+// let a = +prompt("P toping");
+// let P = a * 4 ;
+// alert(P);
+
+// Begon-2
+
+// let a = +prompt("Yuzi ni toping");
+// let S = Math.pow(a, 2);
+// alert("S =" + S);
+
+// Begon-3
+
+// let a = +prompt("S toping");
+// let b = +prompt("P toping");
+// let S = a * b;
+// let P = 2 * (a + b);
+
+// alert(S);
+// alert(P);
+
+// Begin-4
+
+// let d = +prompt("Yuzini toping");
+
+// let L = Math.PI * d;
+
+// alert(L);
+
+// Begin-5
+
+// let a = +prompt("hajmini toping");
+// let V = Math.pow(a, 3);
+// let S = 6 * (a*a);
+
+// alert(V);
+// alert(S);
+
+// Begin-6
+
+// let a = +prompt("V, S aniqlansin");
+// let b = +prompt("V, S aniqlansin");
+// let c = +prompt("V, S aniqlansin");
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+
+// alert(V);
+// alert(S);
+
+// Begin-7
+
+// let R = +prompt(" L va S aniqlansin");
+// let L = 2 * (Math.PI * R);
+// let S = Math.PI * Math.pow(R, 2);
+
+// alert(L);
+// alert(S);
+
+// Begin-8
+
+// let a = +prompt("arifmetigini toping");
+// let b = +prompt("arifmetigini toping");
+
+// let S = (a + b)/2;
+
+// alert(S);
+
+// Begin-13
+
+// let R1 = +prompt("Yuzlarini toping");
+// let R2 = +prompt("Yuzlarini toping");
+
+// let S1 = Math.PI * R1;
+// let S2 = Math.PI * R2;
+// let S3 = Math.PI * (R1 - R2);
+
+// alert(S3);
+
+// Begin-32
+
+// let TF = +prompt("qiymati toping");
+
+// let TC = (TF - 32) * 5/9;
+
+// alert(TC);
+
+
+
+
+
+
+// Integer
+
+// Integer1
+
+// let L = +prompt("fullmetr toping");
+
+// let Fullmetrs = (parseInt (L / 100));
+
+// alert(Fullmetrs);
+
+// Integer2
+
+// let M = +prompt("Fullkg toping");
+
+// let Fullkg = (parseInt (M / 1000));
+
+// alert(Fullkg);
+
+// Integer3
+
+// let File = +prompt("KB toping");
+
+// let FullKB = (parseInt (File / 1024));
+
+// alert(FullKB);
+
+// Integer4
+
+// let A = +prompt("A kesmanda B kesmani nechamarta joylashishi toping");
+// let B = +prompt("A kesmada B kesmani nechamarta joylashishi toping");
+// let Result = (parseInt (A / B ));
+
+// alert(Result);
+
+// Integer5
+
+// let A = +prompt("A kesmanda B kesmani nechamarta joylashishi toping");
+// let B = +prompt("A kesmada B kesmani nechamarta joylashishi toping");
+// let Answer = (parseInt (A / B));
+// let Result = A % B;
+
+// alert(Answer);
+// alert(Result);
+
+// Integer6
+
+// let ikkixonalison = +prompt("ikki xonali son berilgan onli va birli xonlarini toping");
+// let onli = (parseInt (ikkixonalison / 10));
+// let birli = ikkixonalison % 10;
+
+// alert(birli);
+// alert(onli);
+
+// Integer7
+
+// let ikkixonalison = +prompt("ikki xonali sonni yigindisi ni toping");
+// let onli = (parseInt (ikkixonalison / 10));
+// let birli = ikkixonalison % 10;
+// let add = onli + birli;
+
+// alert(add);
+
+// Integer8
+
+// let ikkixonalison = +prompt("ikki xonali sonni urni almashtirish");
+// let onli = (parseInt (ikkixonalison / 10));
+// let birli = ikkixonalison % 10;
+// let exchange = (birli * 10) + onli;
+
+// alert(exchange);
+
+// Integer9
+
+// let uchxonalison = +prompt("uch xonali son yuzli xonasi aniqlansin");
+// let yuzli = (parseInt (uchxonalison / 100));
+
+// alert(yuzli);
+
+// Integer10
+
+// let uchxonalison = +prompt("uch xonali son birli va onli xonasi aniqlansin");
+// let birli = uchxonalison % 10;
+// let onli = (parseInt (uchxonalison / 10) % 10);
+
+// alert(birli);
+// alert(onli);
+
+// Integer11
+
+// let uchxonalison = +prompt("uch xonali son birli va onli xonasi aniqlansin");
+// let birli = uchxonalison % 10;
+// let onli = (parseInt(uchxonalison / 10) % 10);
+// let yuzli = (parseInt(uchxonalison / 100));
+// let add = birli + onli + yuzli;
+
+// alert(add);
+
+// Integer12
+
+// let uchxonalison = +prompt("uch xonali sonni urni almashtirish");
+// let birli = uchxonalison % 10;
+// let onli = (parseInt (uchxonalison / 10) % 10);
+// let yuzli = (parseInt (uchxonalison / 100));
+// let exchange = (birli * 100) + (onli * 10) + yuzli;
+
+// alert(exchange);
+
+// Integer13
+
+// let uchxonalison = +prompt("uch xonali sonni urni almashtirish");
+// let onli = (parseInt (uchxonalison / 10) % 10);
+// let birli = uchxonalison % 10;
+// let yuzli = (parseInt (uchxonalison / 100));
+// let exchange = (onli * 100) + (birli * 10) + yuzli;
+
+// alert(exchange);
